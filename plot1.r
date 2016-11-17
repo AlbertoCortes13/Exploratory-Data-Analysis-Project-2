84b2fa89f6c20177f3fcaf66a9f30ff67a215ab4
AlbertoCortes13 <a.cortes.guerrero@accenture.com>
2016-11-17 11:08:20 -0600

    Add files via upload

---
+++ plot1.r
@@ -0,0 +1,8 @@
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+
+aggregate(Emission ~ year, NEI, sum)
+
+png(filename = "plot1.png", width = 480, height = 480, units = 'px')
+plot(total, xlab = "Year", ylab = "Total Emission From 1999-2008", type = "o")
+dev.off()
