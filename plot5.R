60b0ff9c578785e4fd18ca6737a10e5cae44e742
AlbertoCortes13 <a.cortes.guerrero@accenture.com>
2016-11-23 10:52:11 -0600

    Add files via upload

---
+++ plot5.R
@@ -0,0 +1,13 @@
+library(ggplot2)
+
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+
+baltimore <- NEI[NEI$fips == "24510",]
+motor <- SCC[grep("^Motor", SCC$Short.Name, ignore.case = TRUE), "SCC"]
+motorNEI <- NEI[NEI$SCC %in% motor,]
+total <- aggregate(Emissions ~ year, motorNEI, sum)
+
+png(filename = "plot5.png", width = 480, height = 480, units = 'px')
+qplot(year, Emissions, data = total, geom = c("line", "point"), xlab = "Year", ylab = "Emissions", main = "Emission from Motor Vehicles in Baltimore from 1999-2008")
+dev.off()
